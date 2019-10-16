8be8dcab6df79ebbe214bc35ba64bbf15d6c6e5e
Petry <eric.petry@atos.net>
2019-10-16 14:46:46 +0200

    Project for corporate university Mannheim DE

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ bot.onText(/\/bookmark/, (msg, match) => {
     const url = match.input.split(' ')[1];
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
+    // of the message.
+    
     if (url === undefined) {
         bot.sendMessage(
             chatId,
